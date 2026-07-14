6008129a4f97de8b678ec95e1a263554fa38db5b
Arylen <177881981+Arylen@users.noreply.github.com>
2026-07-14 06:55:19 -0500

    console text works.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <gba.h>
+#include <gba_interrupt.h>
+#include <gba_systemcalls.h>
 #include <stdio.h>
 
 int main() {
+    irqInit();
+    irqEnable(IRQ_VBLANK);
 
+    consoleDemoInit();
+    iprintf("Meow!");
+
+    while (true) {
+        VBlankIntrWait();
+    }
 }
